18df309b67f79350c5c6c7e60cf2190a873de47c
Anthony Tran <anthony.tran6264@yahoo.com>
2021-06-30 21:07:42 -0700

    Completed JS Loops

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -31,8 +31,38 @@ function repeatWord(word, times) {
   var count = 1;
   var repeated = '';
   while (count <= times) {
-    repeated = word;
+    repeated += word + ' ';
     count++;
   }
   return repeated;
+}
+
+function logEachCharacter(string) {
+  for (var i = 0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+}
+
+function doubleAll(numbers) {
+  var doubled = [];
+  for (var i = 0; i < numbers.length; i++) {
+    doubled.push(numbers[i] * 2);
+  }
+  return doubled;
+}
+
+function getKeys(object) {
+  var keys = [];
+  for (var key in object) {
+    keys.push(key);
+  }
+  return keys;
+}
+
+function getValues(object) {
+  var values = [];
+  for (var key in object) {
+    values.push(object[key]);
+  }
+  return values;
 }
