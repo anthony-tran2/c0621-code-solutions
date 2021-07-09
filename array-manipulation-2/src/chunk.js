1622dcfe614f8c616b7148c33e4dc6094bf834e9
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-08 17:29:42 -0700

    Updated array manip 2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,36 @@
 /* exported chunk */
+
+// create output Storage
+// create chunk output
+//  for how many ever items the size value is in the array, starting at the first one:
+//   if chunk item list length is less than count:
+//     add current item to chunk output
+//   otherwise if chunk item list length is equal to count:
+//     add chunk output to output storage
+//     reset chunk output
+//   otherwise:
+//     create remainder output
+//     add remainder to output to output storage
+// return output storage
+
+// function chunk(array, size) {
+//   var newArray = [];
+//   var newChunk = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if (newChunk.length < size) {
+//       newChunk.push(array[i]);
+//     } else if (newChunk.length === size) {
+//       newArray.push(newChunk);
+//       newChunk = [];
+//     }
+//   }
+//   return newArray;
+// }
+
+// function chunk(array, size) {
+//   var newArray = [];
+//   for (var i = 0; i < array.length; i = i + size) {
+//     newArray.push(array.slice(i, i + size));
+//   }
+//   return newArray;
+// }
--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,19 @@
 /* exported includes */
+
+// create storage for output
+// for each item in the Array, starting at the first one:
+//   if the item is strictly equal to the value you input:
+//     -set output to true
+//     -exit the loop
+// return the output storage
+
+function includes(array, value) {
+  var boolean = false;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      boolean = true;
+      break;
+    }
+  }
+  return boolean;
+}
