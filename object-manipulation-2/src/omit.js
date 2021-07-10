4e79ed855ff667501262b39c9eb8fa841b0a573e
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-09 17:17:54 -0700

    Working on object manip 2

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,22 +1,23 @@
 /* exported omit */
 
 // create output Storage
-// for each of your desired keys, starting at the first one:
-//   for each property in your object:
-//     if the value of the property is not strictly equal to undefined:
-//       if the property is equal to the current desired key:
-//         add the current property with its value to your output Storage
+// for property in source, starting at the first one:
+//   for each item in your keys list starting at the first one:
+//     if the current property is equal to current item in keys list:
+//       delete the current property in the output storage
+//       end the loop
+//     add the current property with its value to your output Storage
 // return output storage
 
 function omit(source, keys) {
   var outputStorage = {};
-  for (var i = 0; i < keys.length; i++) {
-    for (var key in source) {
-      if (key !== keys[i]) {
-        if (source[key] === keys[i]) {
-          outputStorage[key] = source[key];
-        }
+  for (var prop in source) {
+    for (var i = 0; i < keys.length; i++) {
+      if (prop === keys[i]) {
+        delete outputStorage[prop];
+        break;
       }
+      outputStorage[prop] = source[prop];
     }
   }
   return outputStorage;
