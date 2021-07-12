2a3438b1077913e92cd537664f9f6b17ca6847ee
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-11 17:05:47 -0700

    Completed Object Manipulation 2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,28 @@
 /* exported defaults */
+
+// make temp storage for target
+// for each property in target object:
+//   add the current target property and value to the temp target storage
+// for each key in source:
+//   add the source properties and their value to the target
+// for each property in target storage:
+//   for each property in source:
+//     if the current property of source is equal to the current property of the temp target storage:
+//       set the current target property equal to the value of the same property in temp target storage
+
+function defaults(target, source) {
+  var tempTarget = {};
+  for (var key in target) {
+    tempTarget[key] = target[key];
+  }
+  for (var key in source) {
+    target[key] = source[key];
+  }
+  for (var prop in tempTarget) {
+    for (var key in source) {
+      if (key === prop) {
+        target[key] = tempTarget[key];
+      }
+    }
+  }
+}
