dd74cafc433b909239997473df0809dffc129954
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-19 11:00:55 -0700

    Working on is-anagram.js

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,8 @@
 /* exported isAnagram */
+
+// create output Storage with a default value of false
+// create storage for altered second string
+// for each character in the first string, starting with the first character:
+//   if the character is not a space:
+//     for each character of the altered second string, strating with the first one:
+//       if the current character of the altered second string is equal to the current character of the first string and it is not a space:
