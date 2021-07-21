fe7996f67e5d29d6f7916799ec950d9af9838ed6
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-20 17:15:19 -0700

    Used a variable instead for clearInterval

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,12 +1,12 @@
 var $h1 = document.querySelector('h1');
-setInterval(countdown, 1000);
+var intervalID = setInterval(countdown, 1000);
 var timer = 4;
 
 function countdown(second) {
   timer -= 1;
   if ($h1.textContent === '1') {
     $h1.textContent = '~Earth Beeeelooowww Us~';
-    clearInterval(1);
+    clearInterval(intervalID);
   } else {
     $h1.textContent = timer;
   }
