7cdd58cca26bd38202e5fde5199d1ab9058e04d0
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-20 17:11:58 -0700

    Created a variable to decrement and cleared the interval at the end of countdown

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,14 +1,13 @@
 var $h1 = document.querySelector('h1');
 setInterval(countdown, 1000);
+var timer = 4;
 
 function countdown(second) {
-  if ($h1.textContent === '4') {
-    $h1.textContent = '3';
-  } else if ($h1.textContent === '3') {
-    $h1.textContent = '2';
-  } else if ($h1.textContent === '2') {
-    $h1.textContent = '1';
-  } else if ($h1.textContent === '1') {
+  timer -= 1;
+  if ($h1.textContent === '1') {
     $h1.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(1);
+  } else {
+    $h1.textContent = timer;
   }
 }
