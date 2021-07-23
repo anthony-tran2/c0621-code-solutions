4250b2e54e3e04c66d1a27d168b9b8c6b1b51c25
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-23 13:06:53 -0700

    Working on title-case

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -13,116 +13,217 @@
 //     add the character to the word storage
 // return output
 
-function titleCase(title) {
-  var output = '';
-  var word = '';
-  var capitalWord = '';
-  for (var i = 0; i < title.length; i++) {
-    if (title[i] === ' ' || title[i] === '-') {
-      if (output === '') {
-        capitalWord += word[0].toUpperCase();
-        for (var o = 1; o < word.length; o++) {
-          capitalWord += word[o];
-        }
-        if (capitalWord.toLowerCase() === 'javascript') {
-          capitalWord = 'JavaScript';
-        } else if (capitalWord.toLowerCase() === 'web') {
-          capitalWord = 'Web';
-        } else if (capitalWord.toLowerCase() === 'api') {
-          capitalWord = 'API';
-        } else if (capitalWord.toLowerCase() === 'javascript:') {
-          capitalWord = 'JavaScript:';
-        }
-        capitalWord += title[i];
-        output += capitalWord;
-        capitalWord = '';
-      } else if (output[output.length - 2] === ':') {
-        capitalWord += word[0].toUpperCase();
-        for (var g = 1; g < word.length; g++) {
-          capitalWord += word[g];
-        }
-        if (capitalWord.toLowerCase() === 'javascript') {
-          capitalWord = 'JavaScript';
-        } else if (capitalWord.toLowerCase() === 'web') {
-          capitalWord = 'Web';
-        } else if (capitalWord.toLowerCase() === 'api') {
-          capitalWord = 'API';
-        }
-        capitalWord += title[i];
-        output += capitalWord;
-        capitalWord = '';
-      } else if (word.length > 3 && title[i] === ' ') {
-        capitalWord += word[0].toUpperCase();
-        for (var c = 1; c < word.length; c++) {
-          capitalWord += word[c];
-        }
-        if (capitalWord.toLowerCase() === 'javascript') {
-          capitalWord = 'JavaScript';
-        } else if (capitalWord.toLowerCase() === 'web') {
-          capitalWord = 'Web';
-        } else if (capitalWord.toLowerCase() === 'api') {
-          capitalWord = 'API';
-        } else if (capitalWord.toLowerCase() === 'javascript:') {
-          capitalWord = 'JavaScript:';
-        }
-        capitalWord += title[i];
-        output += capitalWord;
-        capitalWord = '';
-      } else if (title[i] === '-') {
-        capitalWord += word[0].toUpperCase();
-        for (var d = 1; d < word.length; d++) {
-          capitalWord += word[d];
-        }
-        output += capitalWord;
-        capitalWord = '';
-        if (capitalWord.toLowerCase() === 'javascript') {
-          capitalWord = 'JavaScript';
-        } else if (capitalWord.toLowerCase() === 'web') {
-          capitalWord = 'Web';
-        } else if (capitalWord.toLowerCase() === 'api') {
-          capitalWord = 'API';
-        }
-        capitalWord += title[i];
-        output += capitalWord;
-        capitalWord = '';
-      } else {
-        if (word.toLowerCase() === 'javascript') {
-          word = 'JavaScript';
-        } else if (word.toLowerCase() === 'web') {
-          word = 'Web';
-        } else if (word.toLowerCase() === 'api') {
-          word = 'API';
-        }
-        word += title[i];
-        output += word;
-      }
-      word = '';
-    } else {
-      word += title[i];
-    }
-  }
-  if (word.length > 3) {
-    capitalWord += word[0].toUpperCase();
-    for (var l = 1; l < word.length; l++) {
-      capitalWord += word[l];
-    }
-    if (capitalWord.toLowerCase() === 'javascript') {
-      capitalWord = 'JavaScript';
-    } else if (capitalWord.toLowerCase() === 'web') {
-      capitalWord = 'Web';
-    } else if (capitalWord.toLowerCase() === 'api') {
-      capitalWord = 'API';
-    }
-    output += capitalWord;
-  } else {
-    if (word.toLowerCase() === 'javascript') {
-      word = 'JavaScript';
-    } else if (word.toLowerCase() === 'web') {
-      word = 'Web';
-    } else if (word.toLowerCase() === 'api') {
-      word = 'API';
-    }
-    output += word;
-  }
-  return output;
-}
+// function titleCase(title) {
+//   var output = '';
+//   var words = title.split(' ');
+//   for (var c = 0; c < words.length; c++) {
+//     for (var a = 0; a < words[c].length; a++) {
+//       if (words[c][a] === '-') {
+//         words[c] = words[c].split('-');
+//       }
+//     }
+//   }
+//   var newWord = '';
+//   for (var i = 0; i < words.length; i++) {
+//     if (words[i].length > 1) {
+//       newWord = words[i][0].toUpperCase();
+//       for
+//     }
+//   }
+// }
+
+// function titleCase(title) {
+//   var output = '';
+//   var word = '';
+//   var capitalWord = '';
+//   for (var i = 0; i < title.length; i++) {
+//     if ((title[i] === ' ' && word.length > 2) || title[i] === '-') {
+//       if (output === '') {
+//         capitalWord += word[0].toUpperCase();
+//         for (var o = 1; o < word.length; o++) {
+//           capitalWord += word[o];
+//         }
+//         capitalWord += title[i];
+//       } else if (output[output.length - 2] === ':') {
+//         capitalWord += word[0].toUpperCase();
+//         for (var g = 1; g < word.length; g++) {
+//           capitalWord += word[g];
+//         }
+//         capitalWord += title[i];
+//       } else if (word.length > 3 && title[i] === ' ') {
+//         capitalWord += word[0].toUpperCase();
+//         for (var c = 1; c < word.length; c++) {
+//           capitalWord += word[c];
+//         }
+//         capitalWord += title[i];
+//       } else if (title[i] === '-') {
+//         capitalWord += word[0].toUpperCase();
+//         for (var d = 1; d < word.length; d++) {
+//           capitalWord += word[d];
+//         }
+//         if (capitalWord.toLowerCase() === 'javascript') {
+//           capitalWord = 'JavaScript';
+//         } else if (capitalWord.toLowerCase() === 'web') {
+//           capitalWord = 'Web';
+//         } else if (capitalWord.toLowerCase() === 'api') {
+//           capitalWord = 'API';
+//         } else if (capitalWord.toLowerCase() === 'javascript:') {
+//           capitalWord = 'JavaScript:';
+//         }
+//         output += capitalWord;
+//         capitalWord = '';
+//       } else {
+//         if (word.toLowerCase() === 'javascript') {
+//           word = 'JavaScript';
+//         } else if (word.toLowerCase() === 'web') {
+//           word = 'Web';
+//         } else if (word.toLowerCase() === 'api') {
+//           word = 'API';
+//         }
+//         word += title[i];
+//         output += word;
+//       }
+//       word = '';
+//     } else {
+//       word += title[i];
+//     }
+//   }
+//   if (word.length > 3) {
+//     capitalWord += word[0].toUpperCase();
+//     for (var l = 1; l < word.length; l++) {
+//       capitalWord += word[l];
+//     }
+//     if (capitalWord.toLowerCase() === 'javascript') {
+//       capitalWord = 'JavaScript';
+//     } else if (capitalWord.toLowerCase() === 'web') {
+//       capitalWord = 'Web';
+//     } else if (capitalWord.toLowerCase() === 'api') {
+//       capitalWord = 'API';
+//     }
+//     output += capitalWord;
+//   } else {
+//     if (word.toLowerCase() === 'javascript') {
+//       word = 'JavaScript';
+//     } else if (word.toLowerCase() === 'web') {
+//       word = 'Web';
+//     } else if (word.toLowerCase() === 'api') {
+//       word = 'API';
+//     }
+//     output += word;
+//   }
+//   return output;
+// }
+
+// function titleCase(title) {
+//   var output = '';
+//   var word = '';
+//   var capitalWord = '';
+//   for (var i = 0; i < title.length; i++) {
+//     if (title[i] === ' ' || title[i] === '-') {
+//       if (output === '') {
+//         capitalWord += word[0].toUpperCase();
+//         for (var o = 1; o < word.length; o++) {
+//           capitalWord += word[o];
+//         }
+//         if (capitalWord.toLowerCase() === 'javascript') {
+//           capitalWord = 'JavaScript';
+//         } else if (capitalWord.toLowerCase() === 'web') {
+//           capitalWord = 'Web';
+//         } else if (capitalWord.toLowerCase() === 'api') {
+//           capitalWord = 'API';
+//         } else if (capitalWord.toLowerCase() === 'javascript:') {
+//           capitalWord = 'JavaScript:';
+//         }
+//         capitalWord += title[i];
+//         output += capitalWord;
+//         capitalWord = '';
+//       } else if (output[output.length - 2] === ':') {
+//         capitalWord += word[0].toUpperCase();
+//         for (var g = 1; g < word.length; g++) {
+//           capitalWord += word[g];
+//         }
+//         if (capitalWord.toLowerCase() === 'javascript') {
+//           capitalWord = 'JavaScript';
+//         } else if (capitalWord.toLowerCase() === 'web') {
+//           capitalWord = 'Web';
+//         } else if (capitalWord.toLowerCase() === 'api') {
+//           capitalWord = 'API';
+//         }
+//         capitalWord += title[i];
+//         output += capitalWord;
+//         capitalWord = '';
+//       } else if (word.length > 3 && title[i] === ' ') {
+//         capitalWord += word[0].toUpperCase();
+//         for (var c = 1; c < word.length; c++) {
+//           capitalWord += word[c];
+//         }
+//         if (capitalWord.toLowerCase() === 'javascript') {
+//           capitalWord = 'JavaScript';
+//         } else if (capitalWord.toLowerCase() === 'web') {
+//           capitalWord = 'Web';
+//         } else if (capitalWord.toLowerCase() === 'api') {
+//           capitalWord = 'API';
+//         } else if (capitalWord.toLowerCase() === 'javascript:') {
+//           capitalWord = 'JavaScript:';
+//         }
+//         capitalWord += title[i];
+//         output += capitalWord;
+//         capitalWord = '';
+//       } else if (title[i] === '-') {
+//         capitalWord += word[0].toUpperCase();
+//         for (var d = 1; d < word.length; d++) {
+//           capitalWord += word[d];
+//         }
+//         output += capitalWord;
+//         capitalWord = '';
+//         if (capitalWord.toLowerCase() === 'javascript') {
+//           capitalWord = 'JavaScript';
+//         } else if (capitalWord.toLowerCase() === 'web') {
+//           capitalWord = 'Web';
+//         } else if (capitalWord.toLowerCase() === 'api') {
+//           capitalWord = 'API';
+//         }
+//         capitalWord += title[i];
+//         output += capitalWord;
+//         capitalWord = '';
+//       } else {
+//         if (word.toLowerCase() === 'javascript') {
+//           word = 'JavaScript';
+//         } else if (word.toLowerCase() === 'web') {
+//           word = 'Web';
+//         } else if (word.toLowerCase() === 'api') {
+//           word = 'API';
+//         }
+//         word += title[i];
+//         output += word;
+//       }
+//       word = '';
+//     } else {
+//       word += title[i];
+//     }
+//   }
+//   if (word.length > 3) {
+//     capitalWord += word[0].toUpperCase();
+//     for (var l = 1; l < word.length; l++) {
+//       capitalWord += word[l];
+//     }
+//     if (capitalWord.toLowerCase() === 'javascript') {
+//       capitalWord = 'JavaScript';
+//     } else if (capitalWord.toLowerCase() === 'web') {
+//       capitalWord = 'Web';
+//     } else if (capitalWord.toLowerCase() === 'api') {
+//       capitalWord = 'API';
+//     }
+//     output += capitalWord;
+//   } else {
+//     if (word.toLowerCase() === 'javascript') {
+//       word = 'JavaScript';
+//     } else if (word.toLowerCase() === 'web') {
+//       word = 'Web';
+//     } else if (word.toLowerCase() === 'api') {
+//       word = 'API';
+//     }
+//     output += word;
+//   }
+//   return output;
+// }
