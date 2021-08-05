fa65c1a30348ca503dfbf018eb5447fe379391a9
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-04 18:04:36 -0700

    Working on Card Game

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,22 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+function cardGame(players, cardsPerHand) {
+  var people = [
+    {
+      name: '',
+      hand: []
+    },
+    {
+      name: '',
+      hand: []
+    },
+    {
+      name: '',
+      hand: []
+    },
+    {
+      name: '',
+      hand: []
+    }
+  ];
+}
