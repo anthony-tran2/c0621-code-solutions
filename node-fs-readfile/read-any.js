4fd5fc0ff89a5e518c50f1550d01067852d5d572
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-11 23:56:41 +0000

    Working on cat.js problem

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
 
+const output = [];
+
 for (let i = 2; i < process.argv.length; i++) {
+  output.push(process.argv[i]);
   fs.readFile(process.argv[i], 'utf8', (err, data) => {
     if (err) throw err;
-    console.log(`${process.argv[i]}:
-${data}`);
+    output.forEach((value, index) => {
+      if (value === process.argv[i]) {
+        output[index] = 'wow';
+      }
+    });
   });
+
 }
+
+// output.join('');
+
+console.log(output);
