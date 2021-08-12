deec5e3380067fafa5054babbf7406fc4dba65c0
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-12 20:07:01 +0000

    Simplified cat.js function

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -3,14 +3,9 @@ const fs = require('fs');
 const output = [];
 let counter = 0;
 for (let i = 2; i < process.argv.length; i++) {
-  output.push(process.argv[i]);
   fs.readFile(process.argv[i], 'utf8', (err, data) => {
     if (err) throw err;
-    output.forEach((value, index) => {
-      if (value === process.argv[i]) {
-        output.splice(index, 1, data);
-      }
-    });
+    output[i] = data;
     counter++;
     if (counter === process.argv.length - 2) {
       console.log(output.join('\n'));
