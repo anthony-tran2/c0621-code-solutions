d27080e6e90c0c0a8a1cbbda7943f6e1b229d8e2
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-12 19:33:03 +0000

    User can delete a note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -11,6 +11,19 @@ fs.readFile('./data.json', 'utf8', (err, data) => {
   } else if (process.argv[2] === 'create') {
     notes[`${dataObject.nextId}`] = process.argv[3];
     dataObject.nextId = dataObject.nextId + 1;
+  } else if (process.argv[2] === 'delete') {
+    delete notes[process.argv[3]];
+    dataObject.nextId--;
+    const notesStorage = [];
+    for (const key in notes) {
+      notesStorage.push(notes[key]);
+      delete notes[key];
+    }
+    let counter = 1;
+    for (let i = 0; i < notesStorage.length; i++) {
+      notes[`${counter}`] = notesStorage[i];
+      counter++;
+    }
   }
   const dataJSON = JSON.stringify(dataObject);
   fs.writeFile('./data.json', dataJSON, 'utf8', err => {
