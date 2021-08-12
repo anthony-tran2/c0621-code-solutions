a7f4a7013211c62e93923bc1c560d5adc6e9e868
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-12 19:54:31 +0000

    Completed Node Notes CLI

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,32 +1,31 @@
 const fs = require('fs');
+const dataObject = require('./data.json');
 
-fs.readFile('./data.json', 'utf8', (err, data) => {
-  if (err) throw err;
-  const dataObject = JSON.parse(data);
-  const notes = dataObject.notes;
-  if (process.argv[2] === 'read') {
-    for (const key in notes) {
-      console.log(`${key}: ${notes[key]}`);
-    }
-  } else if (process.argv[2] === 'create') {
-    notes[`${dataObject.nextId}`] = process.argv[3];
-    dataObject.nextId = dataObject.nextId + 1;
-  } else if (process.argv[2] === 'delete') {
-    delete notes[process.argv[3]];
-    dataObject.nextId--;
-    const notesStorage = [];
-    for (const key in notes) {
-      notesStorage.push(notes[key]);
-      delete notes[key];
-    }
-    let counter = 1;
-    for (let i = 0; i < notesStorage.length; i++) {
-      notes[`${counter}`] = notesStorage[i];
-      counter++;
-    }
+const notes = dataObject.notes;
+if (process.argv[2] === 'read') {
+  for (const key in notes) {
+    console.log(`${key}: ${notes[key]}`);
   }
-  const dataJSON = JSON.stringify(dataObject);
-  fs.writeFile('./data.json', dataJSON, 'utf8', err => {
-    if (err) throw err;
-  });
+} else if (process.argv[2] === 'create') {
+  notes[`${dataObject.nextId}`] = process.argv[3];
+  dataObject.nextId = dataObject.nextId + 1;
+} else if (process.argv[2] === 'delete') {
+  delete notes[process.argv[3]];
+  dataObject.nextId--;
+  const notesStorage = [];
+  for (const key in notes) {
+    notesStorage.push(notes[key]);
+    delete notes[key];
+  }
+  let counter = 1;
+  for (let i = 0; i < notesStorage.length; i++) {
+    notes[`${counter}`] = notesStorage[i];
+    counter++;
+  }
+} else if (process.argv[2] === 'update') {
+  notes[`${process.argv[3]}`] = process.argv[4];
+}
+const dataJSON = JSON.stringify(dataObject, null, 2);
+fs.writeFile('./data.json', dataJSON, 'utf8', err => {
+  if (err) throw err;
 });
