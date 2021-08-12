99c01d5763c2c1f0afd6be5e0fe3fd834bdb1cf4
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-12 19:07:42 +0000

    User can now add a note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
 
-let dataJSON = '';
-let dataObject = null;
-
-if (process.argv[2] === 'read') {
-  fs.readFile('./data.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    dataJSON = data;
-    dataObject = JSON.parse(dataJSON);
-    const notes = dataObject.notes;
+fs.readFile('./data.json', 'utf8', (err, data) => {
+  if (err) throw err;
+  const dataObject = JSON.parse(data);
+  const notes = dataObject.notes;
+  if (process.argv[2] === 'read') {
     for (const key in notes) {
       console.log(`${key}: ${notes[key]}`);
     }
+  } else if (process.argv[2] === 'create') {
+    notes[`${dataObject.nextId}`] = process.argv[3];
+    dataObject.nextId = dataObject.nextId + 1;
+  }
+  const dataJSON = JSON.stringify(dataObject);
+  fs.writeFile('./data.json', dataJSON, 'utf8', err => {
+    if (err) throw err;
   });
-}
+});
