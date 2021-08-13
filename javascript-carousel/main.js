6c26820ae668edde4b509ce17d06c2c4f9c88012
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-13 00:13:49 +0000

    Completed JS Carousel

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,12 +1,13 @@
 let counter = 0;
 const $allImages = document.querySelectorAll('img');
 const $allDots = document.querySelectorAll('i.fa-circle');
-// const dot1 = document.querySelector('i[position="1"]');
-// const dot2 = document.querySelector('i[position="2"]');
-// const dot3 = document.querySelector('i[position="3"]');
-// const dot4 = document.querySelector('i[position="4"]');
-// const dot5 = document.querySelector('i[position="5"]');
-// const leftArr = document.querySelector('')
+const dot1 = document.querySelector('i[position="1"]');
+const dot2 = document.querySelector('i[position="2"]');
+const dot3 = document.querySelector('i[position="3"]');
+const dot4 = document.querySelector('i[position="4"]');
+const dot5 = document.querySelector('i[position="5"]');
+const leftArr = document.querySelector('i.fa-chevron-left');
+const rightArr = document.querySelector('i.fa-chevron-right');
 
 const imgRotation = () => {
   $allImages[counter].classList.toggle('hidden');
@@ -18,4 +19,41 @@ const imgRotation = () => {
   $allDots[counter].classList.toggle('far');
 };
 
-const intervalId = setInterval(imgRotation, 3000);
+const imgSelection = dot => {
+  clearInterval(intervalId);
+  for (let i = 0; i < $allDots.length; i++) {
+    if ($allDots[i] === dot) {
+      $allDots[i].className = 'fas fa-circle dot-column';
+      $allImages[i].className = '';
+    } else {
+      $allDots[i].className = 'far fa-circle dot-column';
+      $allImages[i].className = 'hidden';
+    }
+  }
+  if (parseInt(dot.getAttribute('position')) === $allDots.length) counter = 0; else counter = parseInt(dot.getAttribute('position')) - 1;
+  intervalId = setInterval(imgRotation, 3000);
+};
+
+let intervalId = setInterval(imgRotation, 3000);
+
+dot1.addEventListener('click', () => imgSelection(dot1));
+dot2.addEventListener('click', () => imgSelection(dot2));
+dot3.addEventListener('click', () => imgSelection(dot3));
+dot4.addEventListener('click', () => imgSelection(dot4));
+dot5.addEventListener('click', () => imgSelection(dot5));
+leftArr.addEventListener('click', () => {
+  clearInterval(intervalId);
+  $allImages[counter].classList.toggle('hidden');
+  $allDots[counter].classList.toggle('fas');
+  $allDots[counter].classList.toggle('far');
+  if (counter === 0) counter = $allDots.length - 1; else counter--;
+  $allImages[counter].classList.toggle('hidden');
+  $allDots[counter].classList.toggle('fas');
+  $allDots[counter].classList.toggle('far');
+  intervalId = setInterval(imgRotation, 3000);
+});
+rightArr.addEventListener('click', () => {
+  clearInterval(intervalId);
+  imgRotation();
+  intervalId = setInterval(imgRotation, 3000);
+});
