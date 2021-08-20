7af55f8fc9e3e984c91312a9ba832ad285026330
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-20 19:49:46 +0000

    Removed copy of arrays in filter.js and completed optional challenge

--- array-filter/filter.js
+++ array-filter/filter.js
@@ -1,17 +1,3 @@
-const newNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-const newNames = [
-  'Ada',
-  'Hedy',
-  'Jean',
-  'Grace',
-  'Evelyn',
-  'Joan',
-  'Elizabeth',
-  'Janese',
-  'Donna'
-];
-
 const evenNumbers2 = input => { if (input % 2) return true; };
 const overFive2 = input => { if (input > 5) return true; };
 const StartWithE2 = input => { if (input.startsWith('E')) return true; };
