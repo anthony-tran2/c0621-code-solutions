1f81cf90cc93e402bab3c3df9a54607960e99c00
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-25 22:29:58 +0000

    Completed React Stopwatch Component

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -4,7 +4,7 @@ export default class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
     this.playPause = this.playPause.bind(this);
-    this.stopwatch = this.stopwatch.bind(this);
+    this.reset = this.reset.bind(this);
     this.state = {
       seconds: 0,
       timer: 'paused'
@@ -13,21 +13,59 @@ export default class Stopwatch extends React.Component {
 
   playPause() {
     this.setState(prev => {
-
+      if (prev.timer === 'paused') {
+        this.timer();
+        return { timer: 'play' };
+      } else {
+        this.clear();
+        return { timer: 'paused' };
+      }
     });
   }
 
-  stopwatch() {
+  reset() {
     this.setState(prev => {
-      if (prev.timer === 'paused') return { seconds: 0 };
+      if (prev.timer === 'paused') {
+        return { seconds: 0 };
+      }
     });
+  }
+
+  increment() {
+    this.setState(prev => ({ seconds: prev.seconds + 1 }));
+  }
+
+  timer() {
+    this.timerId = setInterval(() => this.increment(), 1000);
+  }
+
+  clear() {
+    clearInterval(this.timerId);
   }
 
   render() {
-    return (
-      <div className="row true-center">
-        <i onClick={this.playPause} className="fas fa-play"></i>
-      </div>
-    );
+    if (this.state.timer === 'paused') {
+      return (
+        <div className="row true-center">
+          <div className="text-align-center">
+            <div onClick={this.reset} className="stopwatch">
+              <p className="timer text-align-center margin-0">{this.state.seconds}</p>
+            </div>
+              <i onClick={this.playPause} className="fas fa-play text-align-center"></i>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="height row true-center">
+          <div className="text-align-center">
+            <div className="stopwatch">
+              <p className="timer text-align-center margin-0">{this.state.seconds}</p>
+            </div>
+            <i onClick={this.playPause} className="fas fa-pause text-align-center"></i>
+          </div>
+        </div>
+      );
+    }
   }
 }
