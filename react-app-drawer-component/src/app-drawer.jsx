0621f718eef1ad0165c5b6dbcac0866cf82f1437
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-26 01:02:14 +0000

    Working on App Drawer

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+function CustomAnchor(props) {
+  return (
+    <div className="padding-bottom-1rem">
+      <a>{props.name}</a>
+    </div>
+  );
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      modal: false
+    };
+  }
+
+  handleClick(e) {
+    if (e.target.tagName === 'A' || e.target.tagName === 'I') this.setState(prev => ({ modal: !prev.modal }));
+  }
+
+  render() {
+    if (this.state.modal) {
+      return (
+        <div>
+          <div>
+            <i onClick={this.handleClick} className="fas fa-bars margin-left-half-rem margin-top-half-rem"></i>
+          </div>
+          <div onClick={this.handleClick} className="row modal-height position-fixed tint">
+            <div className="menu-container">
+              <h1 className="margin-left-menu">Watch</h1>
+              <div className="margin-left-menu">
+                <CustomAnchor name="The" />
+                <CustomAnchor name="World" />
+                <CustomAnchor name="Burn" />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div>
+            <i onClick={this.handleClick} className="fas fa-bars margin-left-half-rem margin-top-half-rem"></i>
+          </div>
+          <div onClick={this.handleClick} className="row modal-height position-fixed tint hidden">
+            <div className="menu-container">
+              <h1 className="margin-left-menu">Watch</h1>
+              <div className="margin-left-menu">
+                <CustomAnchor name="The" />
+                <CustomAnchor name="World" />
+                <CustomAnchor name="Burn" />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
---
+++ react-app-drawer-component/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app-drawer.jsx';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
