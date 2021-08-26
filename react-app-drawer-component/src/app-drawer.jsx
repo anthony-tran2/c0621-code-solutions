b62d97d182b4dbb5ff59cd5acc7b62949146071f
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-26 18:22:25 +0000

    Completed Transitions for app drawer component

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+const words = ['The', 'World', 'Burn', 'MUAHAHAHAHA'];
+
 function CustomAnchor(props) {
   return (
     <div className="padding-bottom-1rem menuADiv">
-      <a>{props.name}</a>
+      <a>{props.word}</a>
     </div>
   );
 }
@@ -18,7 +20,7 @@ export default class App extends React.Component {
   }
 
   handleClick(e) {
-    if (e.target.tagName === 'A' || e.target.tagName === 'I' || e.target.className.includes('tint') || e.target.className.includes('menuADiv')) this.setState(prev => ({ modal: !prev.modal }));
+    if (e.target.tagName === 'A' || e.target.tagName === 'I' || e.target.className.includes('on-screen') || e.target.className.includes('menuADiv') || e.target.className.includes('off-screen')) this.setState(prev => ({ modal: !prev.modal }));
   }
 
   render() {
@@ -28,13 +30,11 @@ export default class App extends React.Component {
           <div>
             <i onClick={this.handleClick} className="fas fa-bars margin-left-half-rem margin-top-half-rem"></i>
           </div>
-          <div onClick={this.handleClick} className="row modal-height position-fixed tint">
-            <div className="menu-container">
+          <div onClick={this.handleClick} className="row modal-height on-screen">
+            <div className="menu-container menu-on modal-height">
               <h1 className="margin-left-menu">Watch</h1>
               <div className="margin-left-menu">
-                <CustomAnchor name="The" />
-                <CustomAnchor name="World" />
-                <CustomAnchor name="Burn" />
+                {words.map(value => <CustomAnchor key={value} word={value} />)}
               </div>
             </div>
           </div>
@@ -46,13 +46,11 @@ export default class App extends React.Component {
           <div>
             <i onClick={this.handleClick} className="fas fa-bars margin-left-half-rem margin-top-half-rem"></i>
           </div>
-          <div onClick={this.handleClick} className="row modal-height position-fixed tint hidden">
-            <div className="menu-container">
+          <div onClick={this.handleClick} className="row modal-height off-screen">
+            <div className="menu-container menu-off modal-height">
               <h1 className="margin-left-menu">Watch</h1>
               <div className="margin-left-menu">
-                <CustomAnchor name="The" />
-                <CustomAnchor name="World" />
-                <CustomAnchor name="Burn" />
+                {words.map(value => <CustomAnchor key={value} word={value} />)}
               </div>
             </div>
           </div>
