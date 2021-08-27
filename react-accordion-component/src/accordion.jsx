35caf5d32033ef114a8d96421c22e4fb7d74ae10
Anthony Tran <anthony.tran6264@yahoo.com>
2021-08-27 17:39:27 +0000

    Removed Hard Coding

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,5 +1,39 @@
 import React, { useState } from 'react';
 
+const info = [
+  {
+    tab: 'HTML',
+    language: 'Hypertext Markup Language',
+    description: 'Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.'
+  },
+  {
+    tab: 'CSS',
+    language: 'Cascading Style Sheets',
+    description: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document writte in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.'
+  },
+  {
+    tab: 'JavaScript',
+    language: 'JavaScript',
+    description: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.'
+  }
+];
+
+function Header(props) {
+  return (
+    <div data-tab={props.tab} className="header">
+      <h1 data-tab={props.tab}>{props.language}</h1>
+    </div>
+  );
+}
+
+function Paragraph(props) {
+  return (
+    <div data-tab={props.tab} className={props.activeTab === props.tab ? 'text' : 'text hidden'}>
+      <p data-tab={props.tab}>{props.description}</p>
+    </div>
+  );
+}
+
 export default function Accordion() {
 
   const [data, setData] = useState({ tab: '' });
@@ -7,31 +41,17 @@ export default function Accordion() {
   const handleDoubleClick = e => {
     const tabValue = e.target.getAttribute('data-tab');
     if (tabValue) {
-      if (tabValue !== data.tab) setData({ tab: tabValue }); else setData({ tab: '' });
+      tabValue !== data.tab ? setData({ tab: tabValue }) : setData({ tab: '' });
     }
   };
 
   return (
     <div className="row true-center full-height">
       <div onDoubleClick={handleDoubleClick}>
-        <div data-tab="HTML" className="header">
-          <h1 data-tab="HTML">Hypertext Markup Language</h1>
-        </div>
-        <div data-tab="HTML" className={data.tab === 'HTML' ? 'text' : 'text hidden'}>
-          <p data-tab="HTML">Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.</p>
-        </div>
-        <div data-tab="CSS" className="header">
-          <h1 data-tab="CSS">Cascading Style Sheets</h1>
-        </div>
-        <div data-tab="CSS" className={data.tab === 'CSS' ? 'text' : 'text hidden'}>
-          <p data-tab="CSS">Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document writte in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.</p>
-        </div>
-        <div data-tab="JavaScript" className="header">
-          <h1 data-tab="JavaScript">JavaScript</h1>
-        </div>
-        <div data-tab="JavaScript" className={data.tab === 'JavaScript' ? 'text' : 'text hidden'}>
-          <p data-tab="JavaScript">JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.</p>
-        </div>
+        {info.map(obj => {
+          return [<Header key={obj.tab} tab={obj.tab} language={obj.language} />,
+          <Paragraph key={obj.tab} tab={obj.tab} activeTab={data.tab} description={obj.description} />];
+        })}
       </div>
     </div>
   );
